32acf87d99c24e624a87c0086b3bc7137192cc4d
Joseph Henshaw <furiousjoe16@gmail.com>
2024-04-02 06:59:59 +0100

    minor fix

--- apps/user-service/src/modules/user/user.service.ts
+++ apps/user-service/src/modules/user/user.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { UserEntity } from './user.entity';
@@ -32,6 +32,7 @@ export class UserService {
       } catch (error) {
         duplicateErrorHandler(error, { nameReplacements: { users: 'email' } })
         console.error(error)
+        throw new InternalServerErrorException(error)
       }
     })
 
